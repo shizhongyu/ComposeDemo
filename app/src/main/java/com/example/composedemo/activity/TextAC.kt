82a752d98a4b7098ff4f8df44000d1df13008839
microli <3365582168@qq.com>
2023-06-02 11:49:31 +0800

    文本Text

--- app/src/main/java/com/example/composedemo/activity/TextAC.kt
+++ app/src/main/java/com/example/composedemo/activity/TextAC.kt
@@ -2,22 +2,39 @@ package com.example.composedemo.activity
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.selection.DisableSelection
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.example.composedemo.R
 import com.example.composedemo.ui.theme.ComposeDemoTheme
@@ -75,6 +92,42 @@ class TextAC : BaseAC() {
          * 居中对齐
          */
         CenterText()
+
+        /**
+         * 字体阴影
+         */
+        TextShadow()
+
+
+        /**
+         * 文本多样式
+         */
+        MultipleStylesInText()
+
+        /**
+         * 行数上限
+         */
+        LongText()
+
+        /**
+         * 末尾省略
+         */
+        OverflowedText()
+
+        /**
+         * 文本居中
+         */
+        AlignedText()
+
+        /**
+         * 文字选择
+         */
+        SelectableText()
+
+        /**
+         * 部分文字选择
+         */
+        PartiallySelectableText()
     }
 
     @Composable
@@ -97,5 +150,104 @@ class TextAC : BaseAC() {
     fun CenterText() {
         Text("Hello World", textAlign = TextAlign.Center,
             modifier = Modifier.width(150.dp))
+    }
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun TextShadow() {
+        val offset = Offset(5.0f, 10.0f)
+        Text(
+            text = "Hello world!",
+            style = TextStyle(
+                fontSize = 24.sp,
+                shadow = Shadow(
+                    color = Color.Blue,
+                    offset = offset,
+                    blurRadius = 3f
+                )
+            )
+        )
+    }
+
+
+    @Composable
+    fun DifferentFonts() {
+        Column {
+            Text("Hello World", fontFamily = FontFamily.Serif)
+            Text("Hello World", fontFamily = FontFamily.SansSerif)
+        }
+    }
+
+    @Composable
+    fun MultipleStylesInText() {
+        Text(
+            buildAnnotatedString {
+                withStyle(style = SpanStyle(color = Color.Blue)) {
+                    append("H")
+                }
+                append("ello ")
+
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color.Red)) {
+                    append("W")
+                }
+                append("orld")
+            }
+        )
+    }
+
+    @Composable
+    fun LongText() {
+        Text("hello ".repeat(50), maxLines = 2)
+    }
+
+    @Composable
+    fun OverflowedText() {
+        Text("Hello Compose ".repeat(50), maxLines = 2, overflow = TextOverflow.Ellipsis)
+    }
+
+
+    @Composable
+    fun AlignedText() {
+        Text(
+            modifier = Modifier.background(Color.LightGray),
+            text = "Hello World!",
+            style = LocalTextStyle.current.merge(
+                TextStyle(
+                    lineHeight = 2.5.em,
+                    platformStyle = PlatformTextStyle(
+                        includeFontPadding = false
+                    ),
+                    lineHeightStyle = LineHeightStyle(
+                        alignment = LineHeightStyle.Alignment.Center,
+                        trim = LineHeightStyle.Trim.None
+                    )
+                )
+            )
+        )
+    }
+
+    @Composable
+    fun SelectableText() {
+        SelectionContainer {
+            Text("This text is selectable")
+        }
+    }
+
+    @Composable
+    fun PartiallySelectableText() {
+        SelectionContainer {
+            Column {
+                Text("This text is selectable")
+                Text("This one too")
+                Text("This one as well")
+                DisableSelection {
+                    Text("But not this one")
+                    Text("Neither this one")
+                }
+                Text("But again, you can select this one")
+                Text("And this one too")
+            }
+        }
     }
 }
