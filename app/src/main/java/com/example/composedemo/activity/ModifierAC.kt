e8a079a9e6c694c79d21863d80fc3a5e7ed63e96
microli <3365582168@qq.com>
2023-06-10 15:04:00 +0800

    ModifierAC

--- app/src/main/java/com/example/composedemo/activity/ModifierAC.kt
+++ app/src/main/java/com/example/composedemo/activity/ModifierAC.kt
@@ -2,17 +2,23 @@ package com.example.composedemo.activity
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -23,9 +29,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.example.composedemo.R
 import com.example.composedemo.ui.theme.ComposeDemoTheme
 
 
@@ -75,6 +85,7 @@ class ModifierAC : BaseAC() {
             ArtistCard()
             ArtistCard02()
             CircleImage()
+            DemoScreen()
         }
     }
 
@@ -129,6 +140,53 @@ class ModifierAC : BaseAC() {
                 contentDescription = null,
                 contentScale = ContentScale.Crop
             )
+        }
+    }
+
+    @Composable
+    fun DemoScreen() {
+
+        val modifier = Modifier
+            .border(width = 2.dp, color = Color.Black)
+            .padding(all = 10.dp)
+
+        val secondModifier = Modifier.height(100.dp)
+
+        Column(
+            Modifier.padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                "Hello Compose",
+                modifier.then(secondModifier),
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(Modifier.height(16.dp))
+            CustomImage(
+                R.drawable.gugong,
+                Modifier
+                    .padding(16.dp)
+                    .width(270.dp)
+                    .clip(shape = RoundedCornerShape(30.dp)))
+        }
+    }
+
+    @Composable
+    fun CustomImage(image: Int, modifier: Modifier = Modifier) {
+        Image(
+            painter = painterResource(image),
+            contentDescription = null,
+            modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        ComposeDemoTheme() {
+            DemoScreen()
         }
     }
 
